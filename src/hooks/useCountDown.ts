142df928896201bfc1f690ee6a01ee61c97858bb
soheil <soheilrasekh555@gmail.com>
2022-06-24 01:39:22 +0430

    add CountDown hook

---
+++ src/hooks/useCountDown.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function useCountDown(targetDate: Date) {
+  const [timeLeft, setTimeLeft] = useState(targetDate.getTime() - Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(targetDate.getTime() - Date.now());
+    });
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return calculateTimeLeft(timeLeft);
+}
+
+// Calculate time left
+function calculateTimeLeft(timeLeft: number) {
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
