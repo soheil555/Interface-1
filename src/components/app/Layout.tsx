423b99d0eff4f69a3971b8ee95d2eab7cfe7ed18
T-Ph525 <att.pham2017@gmail.com>
2022-07-23 00:44:50 +1000

    background

--- src/components/app/Layout.tsx
+++ src/components/app/Layout.tsx
@@ -3,18 +3,34 @@ import { Container, Image } from "@chakra-ui/react";
 import AppHeader from "../common/Header/AppHeader";
 import Tabs from "./Tabs/Tabs";
 import TopImage from "./Image/TopImage";
-import Background from "/public/images/background.png";
 import React from "react";
 const Layout = (page: ReactElement) => {
   return (
     <>
+ <Image
+        src="/images/background.png"
+        position="fixed"
+        objectFit="cover"
+        top="0"
+        left="0"
+        width="full"
+        height="full"
+        zIndex={-100}
+      />
+ <TopImage />
       <AppHeader />
-      <Container maxW="lg">
-        <TopImage />
-        <Tabs />
+      <Tabs />
+      <Container
+        maxW="container.md"
+        display="flex"
+        justifyContent="center"
+        pt={20}
+        pb={40}
+        pl={{ base: undefined, lg: "8rem" }}
+        opacity={0.9}
+      >
         {page}
       </Container>
-      <Image src={Background} />
     </>
   );
 }
