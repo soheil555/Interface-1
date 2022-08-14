5f9c922ebf3979d1f810cb964b2dccc4e6a39678
Evanʎx <95352755+T-Ph525@users.noreply.github.com>
2022-08-14 17:18:40 +1000

    Update HeroBanner.tsx

--- src/components/home/HeroBanner.tsx
+++ src/components/home/HeroBanner.tsx
@@ -16,7 +16,7 @@ const HeroBanner = () => {
         lineHeight="1"
         fontWeight="black"
       >
-        AXOSWAP <Text color="brand.300">PROJECT</Text>
+        AXOSWAP <Text color="brand.300">DECENTRALIZED EXCHANGE</Text>
       </Heading>
       <Text textAlign="center" fontSize="3xl">
         Project Status: ICO event live on Sushiswap!
