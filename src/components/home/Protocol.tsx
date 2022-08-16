67d74b6b043d4acb68cc3dde8b0a853c7674b7b0
Evanyx <att.pham2017@gmail.com>
2022-08-16 19:49:01 +1000

    homepage

--- src/components/home/Protocol.tsx
+++ src/components/home/Protocol.tsx
@@ -28,53 +28,21 @@ const Protocol = () => {
             variant="subtext"
             w={{ base: "100%", md: "80%" }}
           >
-           Swap tokens, farm liquidity and explore the Metaverse
+           Swap tokens, farm liquidity and explore DeFi cutting-edge technology.
           </Text>
+          <Text fontSize="xxl">Need helps?</Text>
           <Link href="https://docs.axoswap.io" isExternal>
             <Button variant="brand-outline">View our docs</Button>
           </Link>
-          <Text fontSize="s">Coming soon </Text>
-          <Text>Platform content breakdown with DeFi first V-Tuber</Text>
+          <Text fontSize="l">Or</Text>
+          <Button variant="brand-outline">Watch our video</Button>
         </Box>
 
         <Image
           alignSelf="center"
-          w={{ base: 500, md: 400, lg: 600 }}
+          w={{ base: 300, md: 200, lg: 400 }}
           src="/images/protocol-img1.png"
         />
-      </Stack>
-
-      <Stack
-        gap={2}
-        justify="center"
-        direction={{ base: "column", md: "row" }}
-        pt={15}
-        pb={20}
-      >
-        <Box alignSelf="center">
-          <NextLink href="https://rarible.com/axoswapio" passHref>
-            <Link isExternal>
-              <Image w={{ base: 500, md: 700 }} src="/images/nftImg.gif" />
-            </Link>
-          </NextLink>
-        </Box>
-
-        <Flex direction="column" align="center" pt={10}>
-          <Heading textAlign="center" fontWeight="light" size="lg">
-            Axoswap Official NFT Collection
-          </Heading>
-          <Text
-            textAlign="center"
-            my={8}
-            variant="subtext"
-            fontSize={{ base: "lg", md: "xl" }}
-            w={["100%", "80%", "60%"]}
-          >
-          </Text>
-          <Link href="https://nft.axoswap.io" isExternal>
-            <Button variant="brand-outline">Mint Now</Button>
-          </Link>
-        </Flex>
       </Stack>
     </Box>
   );
