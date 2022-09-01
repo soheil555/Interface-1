faf6af6581eba9bee8ea279db1aef6f8a38abcf4
soheil <soheilrasekh555@gmail.com>
2022-09-01 16:08:15 +0430

    protocol styles

--- src/components/home/Protocol.tsx
+++ src/components/home/Protocol.tsx
@@ -1,40 +1,38 @@
-import {
-  Stack,
-  Flex,
-  Button,
-  Text,
-  VStack,
-  useBreakpointValue,
-} from "@chakra-ui/react";
-import "../../theme.ts";
+import { Flex, Button, Text, VStack, Stack, Link } from "@chakra-ui/react";
 
-export default function WithBackgroundImage() {
+export default function Protocol() {
   return (
     <Flex
-      w={"full"}
-      h={"100vh"}
-      backgroundImage={"/images/hero.gif"}
-      backgroundSize={"cover"}
-      backgroundPosition={"center center"}
+      w="full"
+      h="50rem"
+      backgroundImage="/images/hero.gif"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      my={5}
+      rounded="lg"
     >
-      <VStack
-        w={"full"}
-        justify={"center"}
-        px={useBreakpointValue({ base: 4, md: 8 })}
-      >
-        <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
-          <Text
-            color="white"
-            fontWeight={700}
-            lineHeight={1.2}
-            fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-          >
-            Defi's most cutting edge protocol!
-          </Text>
-          <Stack direction={"row"}>
+      <VStack spacing={6} w="full" justify="center" px={{ base: 4, md: 8 }}>
+        <Text
+          textAlign="center"
+          color="white"
+          fontWeight={700}
+          lineHeight={1.2}
+          fontSize={{ base: "3xl", md: "4xl" }}
+        >
+          Defi's most cutting edge protocol!
+        </Text>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          w={{ base: "60%", md: "full" }}
+          justify="center"
+          align="center"
+        >
+          <Link href="https://docs.axoswap.io" isExternal>
             <Button variant="brand">View our docs</Button>
+          </Link>
+          <Link href="#">
             <Button variant="brand-2-outline">Buy AxO Token</Button>
-          </Stack>
+          </Link>
         </Stack>
       </VStack>
     </Flex>
